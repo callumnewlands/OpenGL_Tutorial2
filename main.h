fcfc8f1e3c74469509d93b472bbe53b810cdfa7e
callumnewlands <callumnewlands@googlemail.com>
2018-09-01 17:55:34 +0100

    Added tutorial code

--- main.h
+++ main.h
@@ -6,8 +6,13 @@
 
 // Standard Libraries
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
 
 void onWindowResize(GLFWwindow* window, int width, int height);
+unsigned int getShaderProgram(const GLchar* vertexPath, const GLchar* fragmentPath);
+void checkCompileErrors(unsigned int shader, std::string type);
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
